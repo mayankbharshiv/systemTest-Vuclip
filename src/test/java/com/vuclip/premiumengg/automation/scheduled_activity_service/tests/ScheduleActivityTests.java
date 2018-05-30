ad1cbf7cc05650824317d28438d3f180d661bcc2
Mayank <mayank.bharshiv@afourtech.com>
2018-05-30 14:18:10 +0530

    Signed-off-by: Mayank <mayank.bharshiv@afourtech.com>

--- src/test/java/com/vuclip/premiumengg/automation/scheduled_activity_service/tests/ScheduleActivityTests.java
+++ src/test/java/com/vuclip/premiumengg/automation/scheduled_activity_service/tests/ScheduleActivityTests.java
@@ -9,6 +9,7 @@ import org.testng.annotations.Test;
 import com.vuclip.premiumengg.automation.common.Log4J;
 import com.vuclip.premiumengg.automation.scheduled_activity_service.common.models.ActivityType;
 import com.vuclip.premiumengg.automation.scheduled_activity_service.common.models.PublishConfigRequest;
+import com.vuclip.premiumengg.automation.scheduled_activity_service.common.models.UserSubscriptionRequest;
 import com.vuclip.premiumengg.automation.scheduled_activity_service.common.utils.SASHelper;
 import com.vuclip.premiumengg.automation.scheduled_activity_service.common.utils.SASValidationHelper;
 import com.vuclip.premiumengg.automation.utils.ObjectMapperUtils;
@@ -32,31 +33,30 @@ public class ScheduleActivityTests {
 		sasHelper = new SASHelper();
 		sasValidationHelper = new SASValidationHelper();
 		rand = new Random();
-		productId = rand.nextInt(10);
+		productId = 1;//rand.nextInt(10);
 		partnerId = productId;
-		// TODO: test data cleanup if needed
-		
+		Log4J.getLogger().info("Cleanup Test Data");
+		sasHelper.cleanTestData("product_id=" + String.valueOf(productId));
+
 	}
 
 	@DataProvider(name = "activityType")
 	public Object[][] activityType() {
 		return new Object[][] { { ActivityType.ACTIVATION_TYPE }, { ActivityType.ACTIVATION_RETRY_TYPE },
-				{ ActivityType.CONTENT_SMS_TYPE }, { ActivityType.DEACTIVATION },
-				{ ActivityType.DEACTIVATION_RETRY_TYPE }, { ActivityType.ENGAGEMENT_SMS_TYPE },
-				{ ActivityType.FREETRIAL_RENEWAL_TYPE }, { ActivityType.OPTOUT_SMS_TYPE },
-				{ ActivityType.PRE_RENEWAL_SMS_TYPE }, { ActivityType.RENEWAL_TYPE },
-				{ ActivityType.SYSTEM_CHURN_TYPE }, { ActivityType.WINBACK_TYPE }, };
+				{ ActivityType.DEACTIVATION }, { ActivityType.DEACTIVATION_RETRY_TYPE },
+				{ ActivityType.FREETRIAL_RENEWAL_TYPE }, { ActivityType.RENEWAL_TYPE },
+				{ ActivityType.SYSTEM_CHURN_TYPE }, { ActivityType.WINBACK_TYPE } };
 
 	}
 
-	@Test(dataProvider="activityType")
+	@Test(dataProvider = "activityType")
 	public void setupConfigJob(String activityType) throws Exception {
-		
-		//  create job config for activity types( ex- Activation, deactivation)
+
+		// create job config for activity types( ex- Activation, deactivation)
 		PublishConfigRequest publishConfigRequest = ObjectMapperUtils.readValue(
-						"src/test/resources/configurations/scheduled-activity-service/request/publishConfig.json",
-						PublishConfigRequest.class);
-		
+				"src/test/resources/configurations/scheduled-activity-service/request/publishConfig.json",
+				PublishConfigRequest.class);
+
 		Log4J.getLogger().info("Config JSON Body changed");
 		publishConfigRequest.getProduct().setProductId(productId);
 		publishConfigRequest.getProductPartnerMappings().get(0).setProductId(productId);
@@ -74,9 +74,34 @@ public class ScheduleActivityTests {
 		publishConfigRequest.getBlackouts().get(0).setPartnerId(partnerId);
 		publishConfigRequest.getBlackouts().get(0).setProductId(productId);
 		publishConfigRequest.getRetry().get(0).setActivityType(activityType);
-		Log4J.getLogger().info("Config API Called");	
+		Log4J.getLogger().info("Config API Called");
 		sasValidationHelper.validate_sms_api_response(sasHelper.saveProduct(publishConfigRequest));
 	}
-	
-	
+
+	@Test(dependsOnMethods = "setupConfigJob", enabled = false)
+	public void scheduleActivityTests() throws Exception {
+		UserSubscriptionRequest userSubscriptionRequest = ObjectMapperUtils.readValue(
+				"src/test/resources/configurations/scheduled-activity-service/request/userSubscription.json",
+				UserSubscriptionRequest.class);
+		Log4J.getLogger().info("user subscription JSON Body changed");
+
+		/*
+		 * userSubscriptionRequest.getActivityInfo().setActionType(actionType);
+		 * userSubscriptionRequest.getActivityInfo().setActivityType(activityType);
+		 * userSubscriptionRequest.getActivityInfo().setCurrentSubscriptionState(
+		 * currentSubscriptionState);
+		 * userSubscriptionRequest.getActivityInfo().setPreviousSubscriptionState(
+		 * previousSubscriptionState);
+		 * userSubscriptionRequest.getActivityEvent().setTransactionState(
+		 * transactionState);
+		 */
+		userSubscriptionRequest.getSubscriptionInfo().setProductId(productId);
+		userSubscriptionRequest.getSubscriptionInfo().setPartnerId(partnerId);
+		userSubscriptionRequest.getActivityEvent().setPartnerId(partnerId);
+		userSubscriptionRequest.getActivityEvent().setProductId(productId);
+
+		Log4J.getLogger().info("user subscription API Called");
+		sasValidationHelper.validate_sms_api_response(sasHelper.userSubscription(userSubscriptionRequest));
+	}
+
 }
