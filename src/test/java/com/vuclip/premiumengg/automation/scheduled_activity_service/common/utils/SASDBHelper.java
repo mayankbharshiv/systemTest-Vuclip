9126d15f74fa063d180836f83201a64684c23cf0
rahul sahu <rahul.sahu@vuclip.com>
2018-05-30 15:26:33 +0530

    Signed-off-by: rahul sahu <rahul.sahu@vuclip.com>

---
+++ src/test/java/com/vuclip/premiumengg/automation/scheduled_activity_service/common/utils/SASDBHelper.java
@@ -0,0 +1,10 @@
+package com.vuclip.premiumengg.automation.scheduled_activity_service.common.utils;
+
+public class SASDBHelper {
+
+	/**
+	 * after save product validation form these two table SELECT * FROM
+	 * scheduled_activity_service.product_partner_country_config; SELECT * FROM
+	 * scheduled_activity_service.job_config;
+	 */
+}
