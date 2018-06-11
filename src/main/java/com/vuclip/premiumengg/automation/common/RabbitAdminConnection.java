4bbd15ae60b28f754339cd8dd7f28a0768720880
rahul sahu <rahul.sahu@vuclip.com>
2018-06-11 18:33:01 +0530

    added rabbit admin and added code to deleted all queue which we used for product 1234567. Signed-off-by: rahul sahu <rahul.sahu@vuclip.com>

---
+++ src/main/java/com/vuclip/premiumengg/automation/common/RabbitAdminConnection.java
@@ -0,0 +1,41 @@
+package com.vuclip.premiumengg.automation.common;
+
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+
+/**
+ * @author rahul s
+ */
+public class RabbitAdminConnection {
+
+    private static RabbitAdmin rabbitAdmin;
+
+    private RabbitAdminConnection() {
+    }
+
+    public static RabbitAdmin getRabbitAdminConnection() {
+        if (rabbitAdmin == null) {
+        	
+            System.out.println("========== Creating Rabbit MQ Admin Connection ============");
+            CachingConnectionFactory connectionFactory = new CachingConnectionFactory(Configuration.rabbitMQServer);
+            connectionFactory.setUsername(Configuration.rabbitMQUser);
+            connectionFactory.setPassword(Configuration.rabbitMQPassword);
+            connectionFactory.setPort(Integer.parseInt(Configuration.rabbitMQPort));
+            
+        	 rabbitAdmin = new RabbitAdmin(connectionFactory);
+
+            System.out.println("========== Created Rabbit MQ admin ============");
+
+        }
+        return rabbitAdmin;
+    }
+
+    public static void closeAllConnection() {
+        System.out.println("======== Closing Rabbit MQ Admin connection =========");
+        if(rabbitAdmin!=null)
+        if(rabbitAdmin.getRabbitTemplate()!=null)
+        	if(rabbitAdmin.getRabbitTemplate().getConnectionFactory()!=null)
+        rabbitAdmin.getRabbitTemplate().stop();
+       
+    }
+}
--- src/main/java/com/vuclip/premiumengg/automation/common/RabbitMQConnection.java
+++ src/main/java/com/vuclip/premiumengg/automation/common/RabbitMQConnection.java
@@ -8,27 +8,29 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
  */
 public class RabbitMQConnection {
 
-    private static RabbitTemplate rabbitTemplate;
+	private static RabbitTemplate rabbitTemplate;
 
-    private RabbitMQConnection() {
-    }
+	private RabbitMQConnection() {
+	}
 
-    public static RabbitTemplate getRabbitTemplate() {
-        if (rabbitTemplate == null) {
-            System.out.println("========== Creating Rabbit MQ Template ============");
-            CachingConnectionFactory connectionFactory = new CachingConnectionFactory(Configuration.rabbitMQServer);
-            connectionFactory.setUsername(Configuration.rabbitMQUser);
-            connectionFactory.setPassword(Configuration.rabbitMQPassword);
-            connectionFactory.setPort(Integer.parseInt(Configuration.rabbitMQPort));
-            rabbitTemplate = new RabbitTemplate(connectionFactory);
-            System.out.println("========== Created Rabbit MQ Template ============");
+	public static RabbitTemplate getRabbitTemplate() {
+		if (rabbitTemplate == null) {
+			System.out.println("========== Creating Rabbit MQ Template ============");
+			CachingConnectionFactory connectionFactory = new CachingConnectionFactory(Configuration.rabbitMQServer);
+			connectionFactory.setUsername(Configuration.rabbitMQUser);
+			connectionFactory.setPassword(Configuration.rabbitMQPassword);
+			connectionFactory.setPort(Integer.parseInt(Configuration.rabbitMQPort));
+			rabbitTemplate = new RabbitTemplate(connectionFactory);
+			System.out.println("========== Created Rabbit MQ Template ============");
 
-        }
-        return rabbitTemplate;
-    }
+		}
+		return rabbitTemplate;
+	}
 
-    public static void closeAllConnection() {
-        System.out.println("======== Closing Rabbit MQ Connections =========");
-        rabbitTemplate.stop();
-    }
+	public static void closeAllConnection() {
+		System.out.println("======== Closing Rabbit MQ Connections =========");
+		if (rabbitTemplate != null)
+			if(rabbitTemplate.getConnectionFactory()!=null)
+			rabbitTemplate.stop();
+	}
 }
