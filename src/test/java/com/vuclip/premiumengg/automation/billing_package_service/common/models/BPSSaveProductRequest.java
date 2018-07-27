76ecfb4ac1a585bf99c122ffe9bdd4e2b24e9af0
rahul sahu <rahul.sahu@vuclip.com>
2018-07-27 12:40:50 +0530

    Fixed failing TCs.
    Changed save product, country, partner pojos.
    
    Signed-off-by: rahul sahu <rahul.sahu@vuclip.com>

--- src/test/java/com/vuclip/premiumengg/automation/billing_package_service/common/models/BPSSaveProductRequest.java
+++ src/test/java/com/vuclip/premiumengg/automation/billing_package_service/common/models/BPSSaveProductRequest.java
@@ -1,57 +1,28 @@
 package com.vuclip.premiumengg.automation.billing_package_service.common.models;
 
+import java.util.List;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-import java.util.List;
+import groovy.transform.ToString;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder({
-        "product",
-        "pricePoints"
-})
+@JsonPropertyOrder({ "product", "pricePoints" })
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class BPSSaveProductRequest {
 
-    @JsonProperty("product")
-    private Product product;
-    @JsonProperty("pricePoints")
-    private List<PricePoint> pricePoints = null;
-
-    /**
-     * No args constructor for use in serialization
-     */
-    public BPSSaveProductRequest() {
-    }
-
-    /**
-     * @param pricePoints
-     * @param product
-     */
-    public BPSSaveProductRequest(Product product, List<PricePoint> pricePoints) {
-        super();
-        this.product = product;
-        this.pricePoints = pricePoints;
-    }
-
-    @JsonProperty("product")
-    public Product getProduct() {
-        return product;
-    }
-
-    @JsonProperty("product")
-    public void setProduct(Product product) {
-        this.product = product;
-    }
-
-    @JsonProperty("pricePoints")
-    public List<PricePoint> getPricePoints() {
-        return pricePoints;
-    }
-
-    @JsonProperty("pricePoints")
-    public void setPricePoints(List<PricePoint> pricePoints) {
-        this.pricePoints = pricePoints;
-    }
+	@JsonProperty("product")
+	private BPSProduct product;
+	@JsonProperty("pricePoints")
+	private List<BPSPricePoint> pricePoints = null;
 
 }
