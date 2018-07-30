e607b53451bb029e8da8535763fbd1fbd2f1033e
rahul sahu <rahul.sahu@vuclip.com>
2018-07-30 18:35:04 +0530

    Signed-off-by: rahul sahu <rahul.sahu@vuclip.com>

--- src/test/java/com/vuclip/premiumengg/automation/ad_network_service/common/utils/ANSRedisUtils.java
+++ src/test/java/com/vuclip/premiumengg/automation/ad_network_service/common/utils/ANSRedisUtils.java
@@ -12,51 +12,36 @@ public class ANSRedisUtils {
 	public static boolean keyPresent(String transactionId) {
 		Log4J.getLogger("REDIS KEYS CHECK ").info("CHECKING FOR KEY" + getKeyPattern(transactionId));
 		boolean isFound = false;
-		int count =1;
-		Set<String> s=null;
+		int count = 1;
+		Set<String> s = null;
 		try {
-			while (!isFound && count<=3) {
+			while (!isFound && count <= 3) {
 				Thread.sleep(1000);
-			 s = RedisTemplateConnection.getRedisConnection().keys(getKeyPattern(transactionId));
+				s = RedisTemplateConnection.getRedisConnection().keys(getKeyPattern(transactionId));
 				Log4J.getLogger("REDIS KEYS CHECK ").info(s.size());
-				for (String string : s) {
-					Log4J.getLogger("REDIS KEYS CHECK ").info(string);
-				}
-				if (s!=null || (s!=null && s.size()>=1)) {
-					isFound=true;
+				if (s != null || (s != null && s.size() >= 1)) {
+					isFound = true;
 				}
 				count++;
 			}
+			if (s == null)
+				return false;
+
 			if (s.isEmpty())
 				return false;
 			else
 				return true;
+
 		} catch (Exception e) {
 			Log4J.getLogger("REDIS KEYS CHECK ").info("GOT Exception" + e.getMessage());
 			e.printStackTrace();
 		}
 		return false;
-		// boolean keyFound = false;
-		// int lookupCount = 0;
-		// while (!keyFound && lookupCount < 1) {
-		// try {
-		// Thread.sleep(2000);
-		// } catch (InterruptedException e) {
-		// e.printStackTrace();
-		// }
-		// if (util.keys(getKeyPattern(transactionId),
-		// RedisConnection.getRedisConnection()) != null) {
-		// keyFound = true;
-		// break;
-		// }
-		// lookupCount++;
-		// }
-		// return keyFound;
+
 	}
 
 	public static boolean keyNotPresent(String transactionId) {
 		return keyPresent(transactionId);
-		// return true;
 	}
 
 	private static String getKeyPattern(String transactionId) {
@@ -64,15 +49,6 @@ public class ANSRedisUtils {
 		if (org.springframework.util.StringUtils.hasText(transactionId)) {
 			key.append("_").append(transactionId);
 			return key.append("*").toString();
-		}
-		return null;
-	}
-
-	private static String getkey(String transactionId) {
-		StringBuilder key = new StringBuilder("ADNETWORK_DETAILS");
-		if (org.springframework.util.StringUtils.hasText(transactionId)) {
-			key.append("_").append(transactionId);
-			return key.toString();
 		}
 		return null;
 	}
